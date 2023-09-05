9f6278ef10b19f9962317b6f1ac6a9d05000c34f
MManzo01 <144158658+MManzo01@users.noreply.github.com>
2023-09-05 16:54:56 -0600

    Add files via upload

---
+++ L1-MM_1167023.py
@@ -0,0 +1,22 @@
+#("Pensamiento computacional sección 17 laboratorio")
+#(5 de septiembre 2023)
+#Información
+#Declarar variables
+N1=10
+N2=5
+
+#Pedir datos
+N1=int(input("Ingrese el número 1:"))
+N2=int(input("Ingrese el número 1:"))
+
+#Operaciones y resultados
+Suma= N1+N2
+Resta= N1-N2 
+Multiplicación= N1*N2 
+División= N1/N2
+
+#Mostrar resultados
+print("la suma es:",Suma) 
+print("La resta es:",Resta)
+print("La multiplicación es:",Multiplicación)
+print("La división es:",División)
