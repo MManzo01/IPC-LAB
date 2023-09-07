923a48dad8554927ed5b00201245c16ac11029a2
MManzo01 <144158658+MManzo01@users.noreply.github.com>
2023-09-07 17:46:34 +0200

    Add files via upload

---
+++ TS5-MML-1167023.py
@@ -0,0 +1,20 @@
+#Introduccion aa la programacion trabajo supervisado
+#7/09/23
+#Mariano Manzo Lopez
+#Objetivo: Almacenar variables tipo String y mostrar la informacion recibida
+#Entrada: Nombre, edad, carrera y carne
+#Procesos principales: pedir variables, almacenar datos
+#Salida: Mostrar: nombre, edad, carrera y carne
+
+print("Mi segundo programa")
+sNombre=str(input("Porfavor, ingrese su nombre"))
+sEdad=str(input("Por favor, ingrese su edad"))
+sCarrera=str(input("Por favor, ingrese su carrera"))
+sCarne=str(input("Por favor, ingrese su carne"))
+
+print("NOMBRE", sNombre)
+print("EDAD", sEdad)
+print("CARRERA", sCarrera)
+print("CARNE", sCarne)
+
+print("Soy",sNombre,"Tengo", sEdad,"Años y estudio la carrera de", sCarrera, "Mi numero de carne es", sCarne)
